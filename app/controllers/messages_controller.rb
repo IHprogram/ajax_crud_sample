17b265778bdd694a24bbff172fb028bf16ec2542
IHprogram <ihm20202020@gmail.com>
2020-10-30 00:57:52 +0900

    投稿したメッセージの編集・更新機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,18 @@
 class MessagesController < ApplicationController
-  before_action :set_message, only: :destroy
+  before_action :set_message, only: [:edit,:update,:destroy]
   def index
-    @messages = Message.all
+    @messages = Message.all.order(:id)
   end
 
   def create
     @message = Message.create(message_params)
+  end
+
+  def edit
+  end
+
+  def update
+    @message.update(message_params)
   end
 
   def destroy
